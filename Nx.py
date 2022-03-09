68e051ca70f6d04fa3fad9938c2db2332ccdbdce
Micron588 <95529730+micronmic588@users.noreply.github.com>
2022-03-09 12:28:28 +0600

    Create Nx.py

---
+++ Nx.py
@@ -0,0 +1,19 @@
+import os, platform,time
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+from time import sleep
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from nx import __start__
+    print("\n Congratulations! Your device supported!\n")
+    time.sleep(3)
+    __start__()
+elif bit == '32bit':
+    from nx import __start__
+    print("\n Opps! Your device is not supported!\n")
+    time.sleep(10)
+    __start__()
+ 
